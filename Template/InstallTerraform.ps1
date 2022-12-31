d56831860949500345d6b5e8a98b2949e9b3871d
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2022-12-31 19:35:17 +0100

    Create InstallTerraform.ps1

---
+++ Template/InstallTerraform.ps1
@@ -0,0 +1,7 @@
+# DownloadInstall Chocolatey as PackageManager
+# Change SecurityProtocol for downloading from Chocolatey
+[System.Net.ServicePointManager]::SecurityProtocol = 'Tls12'
+Invoke-Expression ( New-Object System.Net.WebClient ).DownloadString( 'https://chocolatey.org/install.ps1' )
+# Install Terraform Latest Version
+Invoke-Expression -Command '& C:\ProgramData\chocolatey\choco install terraform -y -f'
+# 
