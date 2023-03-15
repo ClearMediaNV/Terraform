b0f41a856b9d2e414df62b574075d7d9a7437033
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2023-03-15 18:36:32 +0100

    Update main.tf

--- Template/main.tf
+++ Template/main.tf
@@ -52,7 +52,7 @@ resource "vcd_vapp_vm" "vm1" {
   power_on = false
   cpu_hot_add_enabled = true
   memory_hot_add_enabled = true
-  catalog_name = "Public_Catalog"
+  catalog_name = var.template_catalog_name
   template_name = var.template_windows_name
   memory = 4096
   cpus = 1
@@ -74,7 +74,7 @@ resource "vcd_vapp_vm" "vm2" {
   power_on = false
   cpu_hot_add_enabled = true
   memory_hot_add_enabled = true
-  catalog_name  = "Public_Catalog"
+  catalog_name  = var.template_catalog_name
   template_name = var.template_windows_name
   memory = 6144
   cpus = 2
@@ -115,7 +115,7 @@ resource "vcd_vapp_vm" "vm3" {
   power_on = false
   cpu_hot_add_enabled = true
   memory_hot_add_enabled = true
-  catalog_name = "Public_Catalog"
+  catalog_name = var.template_catalog_name
   template_name = var.template_watchguard_name
   memory = 1024
   cpus = 1
